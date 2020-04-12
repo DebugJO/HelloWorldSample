0214a6d476bfda3e5dfa7581b0da4fbdd4189f86
msjo <me@msjo.kr>
2020-04-12 19:42:27 +0900

    Create WinApiWindows.cpp

---
+++ CPlusPlus/WinApiWindows.cpp
@@ -0,0 +1,66 @@
+//gcc hello.cpp -o hello.exe -mwindows
+
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, 
+						 WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+				   LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND 	 hwnd;
+	MSG 	 msg;
+	WNDCLASS WndClass;   
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;		
+	WndClass.cbClsExtra	= 0;		
+	WndClass.cbWndExtra	= 0;		
+	WndClass.hInstance = hInstance;		
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);	
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	
+	WndClass.lpszMenuName = NULL;		
+	WndClass.lpszClassName = "Window Class Name";	
+	RegisterClass(&WndClass);	
+	hwnd = CreateWindow("Window Class Name",
+		"Window Title Name",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		NULL,	
+		NULL,	
+		hInstance,	
+		NULL	 
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);	
+		DispatchMessage(&msg);	
+	}   
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, 
+						 WPARAM wParam, LPARAM lParam)
+{
+	HDC			hdc;
+	PAINTSTRUCT ps;
+	
+	switch (iMsg) 
+	{
+	case WM_CREATE:
+		break; 
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);	
+		TextOut(hdc, 0, 0, "HelloWorld", 10);
+		EndPaint(hwnd, &ps);
+		break; 
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break; 
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
