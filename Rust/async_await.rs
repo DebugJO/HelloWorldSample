b8068ea203e3c10b8a56411b9fe4ae1b9f96571f
msjo <me@msjo.kr>
2021-01-25 21:23:31 +0900

    Update async_await.rs

--- Rust/async_await.rs
+++ Rust/async_await.rs
@@ -17,3 +17,26 @@ async fn main() {
     write_data(b"HelloWorld").await;    
     println!("End...");
 }      
+
+
+// use tokio::io::AsyncWriteExt;
+// use tokio::macros::support::Future;
+// use tokio::net::TcpStream;
+
+// fn write_data<'a>(data: &'a [u8]) -> impl Future<Output = ()> + 'a {
+//     async move {
+//         let stream = TcpStream::connect("127.0.0.1:8080").await;
+//         let _ = match stream {
+//             Ok(mut s) => s.write_all(data).await,
+//             Err(e) => {
+//                 println!("{}", e.to_string());
+//                 Err(e)
+//             }
+//         };
+//     }
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     write_data(b"HelloWorld").await;
+// }
