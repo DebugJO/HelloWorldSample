89b67371a3d1c0c35ee59aa79cb4f1416b63cad9
msjo <me@msjo.kr>
2021-01-26 23:28:07 +0900

    Create Delegate.cs

---
+++ CSharp/Delegate.cs
@@ -0,0 +1,31 @@
+void Main()
+{
+	MyLambda l = new MyLambda();
+	if (l.myDelegate == null)
+	{
+		MyDelegate d = new MyDelegate(l, typeof(MyLambda).GetMethod("Main"));
+		l.myDelegate = d;
+	}
+	
+	l.myDelegate.Invoke().Dump();
+}
+
+public class MyLambda
+{
+	public MyDelegate myDelegate;
+	public int Main() => 123;
+}
+
+public class MyDelegate
+{
+	object _obj;
+	MethodInfo _info;
+
+	public MyDelegate(object obj, MethodInfo info)
+	{
+		_obj = obj;
+		_info = info;
+	}
+
+	public int Invoke() => (int)_info.Invoke(_obj, null);
+}
