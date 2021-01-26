29285d645ca78122e73b7bba7d2cecff84045fbe
msjo <me@msjo.kr>
2021-01-26 23:35:22 +0900

    Update Delegate.cs

--- CSharp/Delegate.cs
+++ CSharp/Delegate.cs
@@ -2,32 +2,32 @@ void Main()
 {
     MyLambda l = new MyLambda();
     if (l.myDelegate == null)
-	{
-		MyDelegate d = new MyDelegate(l, typeof(MyLambda).GetMethod("Main"));
-		l.myDelegate = d;
-	}
+    {
+        MyDelegate d = new MyDelegate(l, typeof(MyLambda).GetMethod("Main"));
+        l.myDelegate = d;
+    }
 	
-	l.myDelegate.Invoke().Dump();
+    l.myDelegate.Invoke().Dump();
 }
 
 public class MyLambda
 {
-	public MyDelegate myDelegate;
-	public int Main() => 123;
+    public MyDelegate myDelegate;
+    public int Main() => 123;
 }
 
 public class MyDelegate
 {
-	object _obj;
-	MethodInfo _info;
+    object _obj;
+    MethodInfo _info;
 
-	public MyDelegate(object obj, MethodInfo info)
-	{
-		_obj = obj;
-		_info = info;
-	}
+    public MyDelegate(object obj, MethodInfo info)
+    {
+        _obj = obj;
+        _info = info;
+    }
 
-	public int Invoke() => (int)_info.Invoke(_obj, null);
+    public int Invoke() => (int)_info.Invoke(_obj, null);
 }
 
 // [Reference] Raw Coding, "C# Delegates Explained +Func +Action +Closure", https://www.youtube.com/watch?v=KaxNwGA9fiY
