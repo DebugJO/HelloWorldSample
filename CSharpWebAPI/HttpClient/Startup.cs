917a03c86f46345a5c9b61091b92399c3c8704dd
msjo <me@msjo.kr>
2021-08-02 05:07:10 +0900

    Create Startup.cs

---
+++ CSharpWebAPI/HttpClient/Startup.cs
@@ -0,0 +1,54 @@
+using HttpClientExam.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HttpClientExam
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            // [1]
+            services.AddHttpClient();
+            // [2] System.Net.Http.Json 패키지 추가
+            // [3] Models 폴더추가 -> WeatherModel.cs 추가
+            // [4] NavMenu.razor 페이지 추가
+            // [5] _Imports.razor 패키지 추가
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
