079cb16ad9b95de79fe24322d7cc942e92f261c9
msjo <me@msjo.kr>
2021-08-10 16:10:54 +0900

    Create HelloWorld.cs

---
+++ CSharp/InvokeMethodLoadDLL/HelloWorld.cs
@@ -0,0 +1,18 @@
+// DLL Project
+namespace TestLibrary
+{
+    public class HelloWorld
+    {
+        public static string Hello(string str)
+        {
+            return "Hello " + str;
+        }
+    }
+}
+
+/*
+    Dll file : TestLibrary.dll
+    class : HelloWorld
+    Namespace : TestLibrary
+    Method : Hello
+*/
